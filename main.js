732758f16be18b5d4e51d33eec7cecae88197e4d
Huong Dang <miley.dawn@codeimmersives.com>
2020-07-23 00:11:56 -0400

    HW

--- main.js
+++ main.js
@@ -1,15 +1,55 @@
-const makeDino = function() {
+const makeDino = function(name, time, diet, extinct) {
+  if (extinct === undefined) {
+    return {
+      species: name,
+      period: time,
+      carnivore: diet,
+      extinct: false    
+    }
+  } else {
+    return {
+      species: name,
+      period: time,
+      carnivore: diet,
+      extinct: extinct    
+    }
+  }
+
+
 }
 
-const makeSingular = function() {
+const makeSingular = function(obj) {
+  let cloneObj = Object.assign({}, obj)
+  let name = cloneObj.species
+  if (name.slice(name.length-2) === 'us') {
+    cloneObj.species = name.slice(0, name.length-2)
+  }
+  return cloneObj
 }
 
-const truncateSpecies = function() {
+const truncateSpecies = function(obj) {
+  let cloneObj = Object.assign({}, obj)
+  let name = cloneObj.species
+  if (name.length > 7) {
+    cloneObj.species = name.slice(0, 7) +'...'
+  }
+  return cloneObj
 }
 
-const makeExtinct = function() {
+const dino = {
+  species: 'Brachiosaurus',
+  period: 'Jurassic',
+  carnivore: false,
+  extinct: false
+};
 
+
+const makeExtinct = function(obj) {
+let cloneObj = Object.assign({}, obj)
+cloneObj.extinct = true
+return cloneObj
 }
+
 
 module.exports = {
   makeDino,
